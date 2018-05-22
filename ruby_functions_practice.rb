9d43f69a87d4c3a5d1e409b9eb949289cb93ea31
leeburgess67 <leeburgessmail@gmail.com>
2018-05-22 16:20:41 +0100

    3rd Test passed

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -5,10 +5,10 @@ end
 def add(no_1, no_2)
   return no_1 + no_2
 end
-#
-# def subtract(no_1, no_2)
-#   return no_1 - no_2
-# end
+
+def subtract(no_1, no_2)
+  return no_1 - no_2
+end
 #
 # def multiply(no_1, no_2)
 #   return no_1 * no_2
