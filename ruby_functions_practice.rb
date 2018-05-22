a8269d07f77a64364e9e7c3d1f2058463ea39495
leeburgess67 <leeburgessmail@gmail.com>
2018-05-22 16:17:15 +0100

    First test past

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -2,12 +2,22 @@ def return_10
   return 10
 end
 
-def add(no_1, no_2)
-  return no_1 + no_2
-end
-
-def subtract(no_1, no_2)
-  return no_1 - no_2
-end
-
-def multiply(no_1, no_2)
+# def add(no_1, no_2)
+#   return no_1 + no_2
+# end
+#
+# def subtract(no_1, no_2)
+#   return no_1 - no_2
+# end
+#
+# def multiply(no_1, no_2)
+#   return no_1 * no_2
+# end
+# 
+# def divide(no_1, no_2)
+#   return no_1 / no_2
+# end
+#
+# def length_of_string(string)
+#   return
+# end
