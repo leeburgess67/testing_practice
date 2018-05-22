db5f4f3448b76524e2ae442c2f21bfe440516adc
leeburgess67 <leeburgessmail@gmail.com>
2018-05-22 16:22:04 +0100

    5th Test passed

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -14,10 +14,10 @@ def multiply(no_1, no_2)
   return no_1 * no_2
 end
 
-# def divide(no_1, no_2)
-#   return no_1 / no_2
-# end
-#
+def divide(no_1, no_2)
+  return no_1 / no_2
+end
+
 # def length_of_string(string)
 #   return string.length
 # end
